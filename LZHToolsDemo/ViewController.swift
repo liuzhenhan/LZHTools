7c58df0b913bb78a12ea1df0d6ad27eeb5577e84
lzh0428 <497108685@qq.com>
2021-04-07 18:26:02 +0800

    add

--- LZHToolsDemo/ViewController.swift
+++ LZHToolsDemo/ViewController.swift
@@ -11,7 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+          let textView = UITextView(frame:CGRect(x:10, y:20, width:300, height:100))
+          //要显示的文字
+          let str = "欢迎访问hangge.com 欢迎访问hangge.com 欢迎访问hangge.com"
+          //通过富文本来设置行间距
+          let paraph = NSMutableParagraphStyle()
+          //将行间距设置为28
+          paraph.lineSpacing = 20
+          //样式属性集合
+        let attributes = [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 15),
+                          NSAttributedString.Key.paragraphStyle: paraph]
+          textView.attributedText = NSAttributedString(string: str, attributes: attributes)
+          self.view.addSubview(textView)
+
     }
 
 
